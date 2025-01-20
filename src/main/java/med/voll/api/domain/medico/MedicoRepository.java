f324f9c43c203c140dc0778ae023357509690ab1
Nicolas Alves <nicke.rodrigues1125@gmail.com>
2025-01-20 10:27:15 -0300

    initialized springdocs :card_file_box:
    Signed-off-by: Nicolas Alves <nicke.rodrigues1125@gmail.com>

--- src/main/java/med/voll/api/domain/medico/MedicoRepository.java
+++ src/main/java/med/voll/api/domain/medico/MedicoRepository.java
@@ -10,25 +10,24 @@ import org.springframework.data.jpa.repository.Query;
 import java.time.LocalDateTime;
 
 public interface MedicoRepository extends JpaRepository<Medico, Long> {
-    Page<Medico> findAllByAtivoTrue(Pageable paginacao);
+
+    Page<Medico> findAllByAtivoTrue(Pageable pageable);
 
     @Query("""
             select m from Medico m
             where
-            m.ativo = 1
+            m.ativo = true
             and
             m.especialidade = :especialidade
             and
-            m.id not in(
+            m.id not in (
                 select c.medico.id from Consulta c
                 where
                 c.data = :data
                 and
                 c.motivoCancelamento is null
             )
-            order by rand()
-            limit 1
-        """)
+            """)
     Medico escolherMedicoAleatorioLivreNaData(Especialidade especialidade, @NotNull @Future LocalDateTime data);
 
     @Query("select m.ativo from Medico m where m.id = :id")
